d9e40d2a0802b97f8a4f9e601095bdd2e5d70c7f
Abhishek Rai <abhishekraikhaling@gmail.com>
2026-05-24 19:07:33 +0545

    Add email confirmation feature

---
+++ README.md
@@ -0,0 +1,2 @@
+Project Name: Cinema Ticket
+Project Author: Abhishek Rai
--- src/main.c
+++ src/main.c
@@ -9,12 +9,15 @@
 
 int main(){
     char buy_ticket = show_screen(); 
+    char email[50];
 
     FILE *fp;
 
     fp = fopen("bill.txt", "w");
 
     if(buy_ticket == 'y' || buy_ticket == 'Y'){
+        printf("Enter your email: ");
+        scanf("%s", email);
         ticket();
     }else{
         printf("Have a nice day :)\n");
@@ -24,6 +27,8 @@ int main(){
     pay();
 
     fprintf(fp, "===== CINEMA RECEIPT =====\n");
+    fprintf(fp, "\nThis receipt is the confirmation of payment by %s.\n", email);
+    fprintf(fp, "Movie: Lion King\n");
     fprintf(fp, "Ticket Total: Rs.%d\n", total_ticket_price);
     fprintf(fp, "Food Total: Rs.%d\n", total_food_price);
     fprintf(fp, "Grand Total: Rs.%.2f\n", grand_total);
